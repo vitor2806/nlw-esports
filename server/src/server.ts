15b425f75a958789d6f84e969437ded048e05e72
Vitor Rafael <rqfvitor@gmail.com>
2022-09-13 19:00:28 -0300

    initial setup

---
+++ server/src/server.ts
@@ -0,0 +1,8 @@
+// In newer node versions its possible to use 'import' method instead of require, for this, I must add "type":"module" in package.json
+import express from 'express';
+
+const app = express();
+
+app.get();
+
+app.listen(3333, () => console.log('Running...'));
