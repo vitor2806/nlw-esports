871b80b963cd24e9cc4c5400bb7562332f4a5a80
Vitor Rafael <rqfvitor@gmail.com>
2022-09-18 08:58:07 -0300

    feat: add sqlite-prisma, add routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,99 @@
 // In newer node versions its possible to use 'import' method instead of require, for this, I must add "type":"module" in package.json
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get();
+app.use(express.json());
+
+/**
+ * Query params: 'localhost:3333/ads?page=3&sort=content keep page attributes, if i share this link it will keep the info that I'm in page 3
+ */
+
+//Get all games route
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return res.json(games);
+});
+
+//Get all ads from a game route
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id; // get url id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourEnd: true,
+      hourStart: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return res.json(
+    ads.map(ad => {
+      return {
+        ...ad,
+        WeekDays: ad.weekDays.split(','),
+      };
+    })
+  );
+});
+
+//Get discord tag of an ad
+app.get('/ads/:id/discord', async (req, res) => {
+  const adId = req.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.json({
+    discord: ad.discord,
+  });
+});
+
+app.post('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
+  const body: any = req.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return res.status(201).json(ad);
+});
 
 app.listen(3333, () => console.log('Running...'));
