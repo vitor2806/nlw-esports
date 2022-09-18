9c9dec17ff6881b5439ddfce96b22d8b80fff8e9
Vitor Rafael <rqfvitor@gmail.com>
2022-09-18 09:23:05 -0300

    feat: add hour/minutes function handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 // In newer node versions its possible to use 'import' method instead of require, for this, I must add "type":"module" in package.json
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors());
 
 /**
  * Query params: 'localhost:3333/ads?page=3&sort=content keep page attributes, if i share this link it will keep the info that I'm in page 3
@@ -53,6 +56,8 @@ app.get('/games/:id/ads', async (req, res) => {
       return {
         ...ad,
         WeekDays: ad.weekDays.split(','),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
       };
     })
   );
