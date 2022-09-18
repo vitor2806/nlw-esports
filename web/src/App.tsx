7b95279e7709d5bc0a82a463418a6204b5740142
Vitor Rafael <rqfvitor@gmail.com>
2022-09-18 11:41:08 -0300

    feat: add base modal with radix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,71 +1,108 @@
-import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { GameController } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+
 import logoImg from './assets/page-logo.svg';
+import { GameBanner } from './components/GameBanner';
+import { InviteBanner } from './components/InviteBanner';
+
+interface GameProps {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+const BASE_URL = 'http://localhost:3333';
 
 export default function App() {
+  const [games, setGames] = useState<GameProps[]>([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/games`)
+      .then(res => res.json())
+      .then(data => setGames(data));
+  }, []);
+
   return (
     <div className="max-w-[1344px] px-8 2xl:px-0 mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
+
       <h1 className="text-6xl text-white font-black mt-20">
         Your <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> is here.
       </h1>
+
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game1.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game2.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game3.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game4.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game5.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src="/game6.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Game</strong>
-            <span className="text-zinc-300 text-sm block">Ads</span>
-          </div>
-        </a>
+        {games.map(game => {
+          return <GameBanner key={game.id} adsCount={game._count.ads} bannerUrl={game.bannerUrl} title={game.title} />;
+        })}
       </div>
 
-      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Didn't find your duo?</strong>
-            <span className="text-zinc-400 block">Post an invite to find new mates!</span>
-          </div>
+      <Dialog.Root>
+        <InviteBanner />
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-colors text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Post invite
-          </button>
-        </div>
-      </div>
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed flex items-center justify-center">
+            <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white rounded-lg w-[480px] shadow-lg shadow-black/25">
+              <Dialog.Title className="text-3xl font-black">Publish an invite</Dialog.Title>
+              <Dialog.Content>
+                <form action="">
+                  <div>
+                    <label htmlFor="game">Which is the game?</label>
+                    <input type="text" id="game" name="game" placeholder="Choose the game you want to play" />
+                  </div>
+
+                  <div>
+                    <label htmlFor="nick">Your name (or nickname)</label>
+                    <input type="text" id="nick" name="nick" placeholder="How are you known ingame?" />
+                  </div>
+
+                  <div>
+                    <div>
+                      <label htmlFor="yearsPlaying">Your years playing</label>
+                      <input type="number" id="yearsPlaying" name="yearsPlaying" placeholder="It's ok to be 0!" />
+                    </div>
+
+                    <div>
+                      <label htmlFor="discord">Your discord tag</label>
+                      <input type="text" id="discord" name="discord" placeholder="ExampleUser#0000" />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <label htmlFor="weekDays">Week days you used to play</label>
+                    </div>
+
+                    <div>
+                      <label htmlFor="hourStart">Hour you used to play</label>
+                      <div>
+                        <input type="time" id="hourStart" name="hourStart" placeholder="From" />
+                        <input type="time" id="hourEnd" name="hourEnd" placeholder="To" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <input type="checkbox" />
+                    I'm used to voice chat
+                  </div>
+
+                  <footer>
+                    <button>Cancel</button>
+                    <button type="submit">
+                      <GameController />
+                      Find duo
+                    </button>
+                  </footer>
+                </form>
+              </Dialog.Content>
+            </Dialog.Content>
+          </Dialog.Overlay>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
---
+++ web/src/components/InviteBanner/index.tsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+export function InviteBanner() {
+  return (
+    <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+        <div>
+          <strong className="text-2xl text-white font-black block">Didn't find your duo?</strong>
+          <span className="text-zinc-400 block">Post an invite to find new mates!</span>
+        </div>
+
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-colors text-white rounded flex items-center gap-3">
+          <MagnifyingGlassPlus size={24} />
+          Post invite
+        </Dialog.Trigger>
+      </div>
+    </div>
+  );
+}
